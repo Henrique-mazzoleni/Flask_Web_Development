ad3bb4e67fe4bd0e229cce0dee6d295469b6d9f4
Henrique Mazzoleni <73548364+Henrique-mazzoleni@users.noreply.github.com>
2021-06-12 17:59:16 +0000

    Added Unittests

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -3,11 +3,13 @@ from app.models import User
 
 class UserModelTestCase(unittest.TestCase):
     def test_password_setter(self):
-        test_user = User(password = 'Cat')
+        password = 'Cat'
+        test_user = User(password=password)
         self.assertTrue(test_user.password_hash is not None)
 
     def test_no_password_getter(self):
-        test_user = User(password = 'Cat')
+        password = 'Cat'
+        test_user = User(password=password)
         with self.assertRaises(AttributeError):
             test_user.password
 
@@ -17,3 +19,9 @@ class UserModelTestCase(unittest.TestCase):
         test_user = User(password=password)
         self.assertTrue(test_user.verify_password(password))
         self.assertFalse(test_user.verify_password(not_password))
+
+    def test_salts_are_random(self):
+        password = 'Cat'
+        test_user = User(password=password)
+        other_test_user = User(password=password)
+        self.assertTrue(test_user.password_hash != other_test_user.password_hash)
