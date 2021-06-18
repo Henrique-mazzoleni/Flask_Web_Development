058f5a8ef48a0601a7fe9793e1c1c22b3de31f97
Henrique Mazzoleni <73548364+Henrique-mazzoleni@users.noreply.github.com>
2021-06-18 13:25:44 +0000

    issues fixed, User Profile page running

--- app/main/views.py
+++ app/main/views.py
@@ -31,6 +31,6 @@ def index():
         current_time=datetime.utcnow())
 
 @main.route('/user/<username>')
-def user(name):
+def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
