344e5cf38c1cdbb78474e7f3ea7d424ca81d4775
Henrique Mazzoleni <73548364+Henrique-mazzoleni@users.noreply.github.com>
2021-06-18 16:21:25 +0000

    Admin Profile edit fixed and functional

--- app/main/views.py
+++ app/main/views.py
@@ -20,17 +20,13 @@ def index():
             db.session.commit()
             session['known'] = False
             if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
-                    'mail/new_user',user=user)
+                send_email(current_app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user',user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('.index'))
-    return render_template('index.html', 
-        form=form, name=session.get('name'),
-        known=session.get('known', False),
-        current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False), current_time=datetime.utcnow())
 
 @main.route('/user/<username>')
 def user(username):
